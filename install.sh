070b4715661a905ba12797274b706c6958921270
Andras Popovics <popovics@ndras.hu>
2026-06-11 20:43:27 +0200

    Yet another try

--- install.sh
+++ install.sh
@@ -21,8 +21,8 @@ function installVimPlug() {
 }
 
 function installSpaceshipPrompt() {
-  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt" --depth=1
+  ln -s "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
   printf "%sSPACESHIP: Spaceship prompt installed.%s\n" "$YELLOW" "$RESET"  
 }
 
